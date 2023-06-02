befa2b62159b26274a19c11e1ddb121320c62bf9
Diego <mantee99@outlook.com>
2023-06-02 14:28:50 +0200

    add lite version

---
+++ lite/index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Diego Mantegazza | Web Designer (:</title>
+    <style>
+        body{
+            margin: 0 auto;
+            padding: 0 1rem;
+        }
+        a{
+            width: fit-content;
+            text-decoration: underline;
+            color: inherit;
+        }
+        section{
+            margin: 2rem 0;
+        }
+    </style>
+</head>
+<body>
+    <main>
+        <h1>
+            I'm Diego, Web Designer
+        </h1>
+        <p>
+            My goal is to design/build modern, easy-to-use and graphically appealing responsive website for both companies and individuals. Get in touch!
+        </p>
+        <p>If you're here it means that JavaScript is disabled on your machine or you just wanted the light version of the website. If you want to see the 
+            normal page, visit this link: <a href="https://diegomantegazza.me">diegomantegazza.me</a>
+        </p>
+        <div style="display: flex; flex-direction: column; gap: .3em;">
+            <a href="">View my Github &#11157;</a>
+            <a href="">View my LinkedIn &#11157;</a>
+            <a href="">View my Telegram &#11157;</a>
+        </div>
+    </main>
+    <section>
+        <h2>A bit about me</h2>
+        <p><strong>Myself</strong>: the field for many years, I recently decided to use my knowledge to develop websites, becoming a web designer. I usually work with methodological approaches, going to meet customer needs with good UIs.</p>
+        <p><strong>Design</strong>:I like to experiment with different styles and layouts, mantaining the right balance between the artistic and functional parts. I'm always on the lookout for new structures and typography, keeping abreast of trends.</p>
+        <p><strong>Code & IT</strong>:I have the right software and hardware tools to develop performant and secure websites. My background allows me to have a broad knowledge of computer science, both the programming side and the internet side.</p>
+    </section>
+    <section>
+        <span>DESIGN</span>
+        <p>The first phase of the project is to develop the design agreed with the client. If you don't like it, there is still time to change it.</p>
+        <span>DEVELOPMENT</span>
+        <p>Once the design is completed, we move on to the actual development of the website through appropriate technologies.</p>
+        <span>DEPLOYMENT</span>
+        <p>The developed website is deployed on the best hosting services at the lowest possible price, always keeping data security in mind.</p>
+    </section>
+    <section>
+        <h2>Projects showcase</h2>
+        <div>
+            <span>MANTEGAZZA SRL</span>
+            <a href="https://edilmantegazza.it" target="_blank">Visit page &#11157;</a>
+        </div>
+        <div>
+            <span>VALTRIGHE BASKETBALL</span>
+            <a href="https://valtrighebasketball.com" target="_blank">Visit page &#11157;</a>
+        </div>
+        <div>
+            <span>ARKITECH STP</span>
+            <a href="https://arkitechstp.it" target="_blank">Visit page &#11157;</a>
+        </div>
+        <a href="/projects/" style="margin-top: 2rem;">All the projects</a>
+    </section>
+    <!--
+    <section>
+        <h2>Some certification</h2>
+    </section>
+    -->
+</body>
+</html>
